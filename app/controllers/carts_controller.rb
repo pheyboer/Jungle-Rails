44936282b898ea3c933a36254542d11b6ff3529f
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-19 15:16:06 -0400

    updating empty cart ui and redirection

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
 
   def show
+    @enhanced_cart = enhanced_cart
   end
 
   def add_item
@@ -23,6 +24,14 @@ class CartsController < ApplicationController
     cart[product_id] = (cart[product_id] || 0) + delta
     cart.delete(product_id) if cart[product_id] < 1
     update_cart cart
+  end
+
+  def enhanced_cart
+    session[:cart] ||= {}
+  end
+
+  def update_cart(cart)
+    session[:cart] = cart
   end
 
 end

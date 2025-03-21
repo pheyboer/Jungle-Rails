c05c6f82790f4d0c28ff05d9776b0675b567eb73
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-21 13:25:28 -0400

    implementing sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Log in Successful'
+    else
+      flash.now[:alert] = 'Invalid Email or Password'
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'You are now Logged out'
   end
 end

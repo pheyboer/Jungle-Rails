5beb0ea730f31233757eb50cc7b6e9ae2107519c
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-21 11:09:24 -0400

    setting up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Account Created Successfully!'
+    else
+      notice: 'Account was unable to be created'
+      render :new
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+      )
+      
   end
 end

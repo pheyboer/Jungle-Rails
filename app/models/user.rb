4569a893479a442c77fadc09c274b204ab3c464b
Peter Heyboer <heyboer.peter@gmail.com>
2025-03-21 11:02:04 -0400

    setting up user.rb model and migrating database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
-class User < ApplicationRecord
+class User < ApplicationRecord::Base
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  if user && user.authenticate(password)
+    return user
+  else
+    return nil
+  end
+
+
 end

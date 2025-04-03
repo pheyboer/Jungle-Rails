1bc7b838562e3ddad31607c5acd5890b7499e3e0
Peter Heyboer <heyboer.peter@gmail.com>
2025-04-03 15:16:19 -0400

    adding tests in user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    # Test user creation with all fields
+    # testing user creation with all fields
     it 'should create a valid user' do
       user = User.new(
         first_name: "John",
@@ -13,6 +13,31 @@ RSpec.describe User, type: :model do
         password_confirmation: "password"
       )
       expect(user).to be_valid
+    end
+
+    # test for password and password_confirmation match
+    it 'should have matching password and password_confirmation' do
+      user = User.new(
+        first_name: "John",
+        last_name: "Doe",
+        email: "test@example.com",
+        password: "password",
+        password_confirmation: "different"
+      )
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'should require password and password_confirmation fields' do
+      user = User.new(
+        first_name: "John",
+        last_name: "Doe",
+        email: "test@example.com",
+        password: nil,
+        password_confirmation: nil
+      )
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include("Password can't be blank")
     end
   
   

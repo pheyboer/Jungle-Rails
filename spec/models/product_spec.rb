0f43af1087f4ba380e618f3cca2df46955dfdc79
Peter Heyboer <heyboer.peter@gmail.com>
2025-04-03 14:35:01 -0400

    adding tests in product_specs.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe Product, type: :model do
       expect(product).to be_valid
     end
 
+    it 'is not valid without a name' do
+      product = Product.new(
+        name: nil,
+        price: 699.99,
+        quantity: 10,
+        category: @category
+      )
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
+    end
+
 
     
   end

26cfb38ed7f0316cebd9b9ceff1fa8489ecbc1f1
Peter Heyboer <heyboer.peter@gmail.com>
2025-04-03 14:48:56 -0400

    fixing price testing to price_cents

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Product, type: :model do
     it 'saves successfully when all fields are set' do
       product = Product.new(
         name: 'Smartphone',
-        price: 699.99,
+        price_cents: 69999,
         quantity: 10,
         category: @category
       )
@@ -19,7 +19,7 @@ RSpec.describe Product, type: :model do
     it 'is not valid without a name' do
       product = Product.new(
         name: nil,
-        price: 699.99,
+        price_cents: 69999,
         quantity: 10,
         category: @category
       )
@@ -30,7 +30,7 @@ RSpec.describe Product, type: :model do
     it 'is not valid without a price' do
       product = Product.new(
         name: 'Smartphone',
-        price: nil,
+        price_cents: nil,
         quantity: 10,
         category: @category
       )
@@ -41,12 +41,23 @@ RSpec.describe Product, type: :model do
     it 'is not valid without a quantity' do
       product = Product.new(
         name: 'Smartphone',
-        price: 699.99,
+        price_cents: 69999,
         quantity: nil,
         category: @category
       )
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'is not valid without a category' do
+      product = Product.new(
+        name: 'Smartphone',
+        price_cents: 69999,
+        quantity: 10,
+        category: nil
+      )
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
     end
 
 
